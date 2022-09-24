81ccfcc5b5b0722590986cc5807bfa9d5c474de9
Egoshina <selina.anastasia@mail.ru>
2022-09-24 10:39:09 +0300

    добавил файлы первого занятия

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+
+const ourPlanetName = "Земля";
+console.log(ourPlanetName);
+const currentTimeUser = 12;
+let namberArticle = '25';
+let cashNonCash = "да/нет";
+let surname = "Selin";
+let name = "Oleg";
+let patronymic = "Dmitrievic";
+console.log(ourPlanetName, currentTimeUser, namberArticle, cashNonCash, surname, name, patronymic);
