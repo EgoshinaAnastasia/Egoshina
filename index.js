92e9d23884babc5237b36f46bc5d6676b6bc8540
Egoshina <selina.anastasia@mail.ru>
2022-09-30 22:14:15 +0300

    изменения после 2 занятия

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-console.log("hello world");
-
+/*console.log("hello world");
 
 const ourPlanetName = "Земля";
 console.log(ourPlanetName);
 const currentTimeUser = 12;
-let namberArticle = '25';
+let namberArticle = "25";
 let cashNonCash = "да/нет";
+
 let surname = "Selin";
 let name = "Oleg";
 let patronymic = "Dmitrievic";
-console.log(ourPlanetName, currentTimeUser, namberArticle, cashNonCash, surname, name, patronymic);
+console.log(
+  ourPlanetName,
+  currentTimeUser,
+  namberArticle,
+  cashNonCash,
+  surname,
+  name,
+  patronymic
+);
+
+const age = 18;
+
+const userDataName = `имя пользователя: ${name}, возраст ${age}`;
+console.log(userDataName); */
+
+let tourName;
+tourName = "Москва";
+console.log(tourName);
+
+const userDate = {
+  Name: "Tasia",
+  age: 29,
+};
+console.log(userDate.Name);
+console.log(typeof userDate.age);
+console.log(typeof userDate.Name);
