7fea292bafe17bd60cc176ea0e61b4bccdadd9e8
Egoshina <selina.Anastasia@mail.ru>
2022-10-07 14:09:32 +0300

    домашняя работа 2

---
+++ homework2.js
@@ -0,0 +1,21 @@
+console.log("кот" > "код"); //т больше д
+console.log("2" + 2 * "2" ); // сначало идет умножение, а потом добовляем 2 как строку и того 24
+console.log(undefined == null); //потому что не строгое равенство
+console.log(undefined != null); //
+console.log(null == 0); //
+console.log(2 > "3"); //
+console.log(null - false + true); //
+console.log(1 / "l"); // NaN ошибка, мы не можем делить число на строку
+console.log("2" * "3"); // строки приобразуются в цифры
+console.log(4 + 5 + "O" ); // 4 и 5 складываются как цыфры и 0 добавляется как строка
+console.log("l" + 4 + 5 ); // 1 строка, 4 добавляется как строка и 5 тоже уже добавляется как строка
+console.log("4" - 2 ); // 4 преобразуется в цифру и по этому просто считаем как пример
+console.log("4" - "4x"); // из второй не можем вычленить цифру и по этому NaN
+console.log("23" == 23); //
+console.log(null == false); //
+console.log(" -4 "/ 0 + 1); // может при деление на 0 получается бесконечность и +1 = 1
+console.log(null + 1); //
+console.log(undefined + null); //
+console.log(1 === "1"); // у нас сторое равенство а мы пытамся сравнить цифру и строку
+console.log("2" > 10); // 
+console.log(NaN == undefined); //
