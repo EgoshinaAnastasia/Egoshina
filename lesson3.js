7562a384c7e5b8e53af14e956fb4b2f302751dd7
Egoshina Anastasia <selina.anastasia@mail.ru>
2022-10-14 15:42:24 +0300

    3 занятие

---
+++ lesson3.js
@@ -0,0 +1,130 @@
+// if (1) {console.log("true")};
+ 
+// const name = "Alex3";
+// if (name === "Ivan")
+// {console.log(`Привет ${name}`);}
+// else if (name === "Alex")
+// {console.log(`Привет друг ${name}`);}
+// else if (name === "Petr") {
+// console.log(`Hi ${name}`);
+// }
+// else {
+//     console.log("Привет незнакомец");}
+ 
+//   Тернарный оператор
+// if (true) {
+//     if (true) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+ 
+// } else {
+//     console.log("false");}
+ 
+// const userName = "Alex45";
+// const result = userName === "Alex" ? "Hello Alex" : "Hello User ";
+// console.log(result);
+ 
+// const line1 = "100";
+// const line2 = "20";
+// const line3 = "3";
+// if (line1 < line2 && line2 < line3) {
+//   console.log("line3");
+// } else if (line1 < line2 && line2 > line3) {
+//   console.log("line2");
+// } else if (line1 < line3) {
+//   console.log("line3");
+// } else {
+//   console.log("line1");
+// }
+ 
+// if (line1 > line2 > line3) {
+//   console.log(`line1`);
+// } else if (line2 > line1 > line3) {
+//   console.log(`line2`);
+// } else if (line3 > line1 > line2) {
+//   console.log(`line3`);
+// } else if (line2 > line1) {
+//   console.log("line2");
+// } else {
+//   console.log("line1");
+// }
+ 
+// if (line1 < line2) {
+//   line1 < line2 ? console.log(`line3`) : console.log(`line2`);
+// } else {
+//   line1 < line3 ? console.log(`line3`) : console.log(`line1`);
+// }
+ 
+// const UserRole = "admin";
+// switch (UserRole) {
+//   case "admin":
+//     console.log("Это админ, и он любит чай");
+//     break;
+//   case "user":
+//     console.log("Это юзер, и он любит кьолу");
+//     break;
+ 
+//   case "manager":
+//     console.log("Это менеджер, и он любит кофе");
+//     break;
+//     default:
+//         break;
+// }
+ 
+// const t = 40;
+// if (t <= -30) {
+//   console.log("Оставайтесь дома");
+// } else if (t > -30 && t <= -10) {
+//   console.log("Сегодня холодно");
+// } else if (t > -10 && t <= 5) {
+//   console.log("не холодно");
+// } else if (t > 5 && t <= 15) {
+//   console.log("тепло");
+// } else if (t > 15 && t <= 25) {
+//   console.log("очень тепло");
+// } else if (t > 25 && t <= 35) {
+//   console.log("жарко");
+// } else {
+//   console.log("Пекло");
+// }
+ 
+// let i = 0;
+// while (i < 100) {
+//   console.log(i);
+//   i = i + 10;
+//   i +=10;
+// //   i++; только единицу +1 всегда прибавляет
+// }
+ 
+/*Циклы */
+// let i=0
+// while (true) {
+//     if (i<30) {
+//         i+=1;
+//     }else{
+//         break;  /*принудительная остановка цикла */
+//     }
+//     console.log(i);
+// }
+ 
+// const arr = [1,3,5,6,9,12,14,16,17,18,21,25,65,37];
+// const result=[];
+ 
+// for (let i=0; i< arr.length;i+=1) {
+//     if(arr [i]%2 === 0) { /*остаток от деления, нецелые числа*/
+//     result.push(arr[i]);
+//     }
+// }
+// console.log(result);
+ 
+let a = 1;
+ 
+while (true) {
+     if (a<10) {
+       a+=1;
+    }else{
+     break;  
+     }
+     console.log("Hellow"+a);}
